a4033bded28a48bc2fec02325951a27e58873622
TheSola10 <thesola10@bobile.fr>
2020-09-05 17:29:02 +0200

    Updated compinstall path for zstyle automation

--- 01-zstyle.zsh
+++ 01-zstyle.zsh
@@ -14,6 +14,6 @@ zstyle ':completion:*' menu             select=long-list select=0 select=0
 zstyle ':completion:*' original         true
 zstyle ':completion:*' select-prompt    $'%{\e[38;5;232;48;5;81m%} SELECT %{\e[38;5;81;48;5;232m%}%{\e[38;5;232;48;5;67m%}%{\e[38;5;253;48;5;67m%} At %p %{\e[K\e[0m%}'
 zstyle ':completion:*' verbose          true
-zstyle ':compinstall'  filename         '/home/karim/.zshrc'
+zstyle ':compinstall'  filename         "$ZSH_CONFIG_PATH/01-zstyle.zsh"
 
 autoload -Uz compinit       # Enables the smart completion engine configured above
