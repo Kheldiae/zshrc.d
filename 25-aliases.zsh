8a03288e3a770aca4c2d7ba0556c2cea232d62ea
TheSola10 <thesola10@bobile.fr>
2021-02-11 17:44:56 +0100

    Cleaned up aliases a bit

--- 25-aliases.zsh
+++ 25-aliases.zsh
@@ -1,13 +1,13 @@
 alias sudo="sudo "                  # Important, makes aliases work with sudo
 
 alias ducks="du -cksh"              # Shows disk usage by element in one tree lvl
+alias btrfs-ducks="btrfs filesystem du -s --human-readable"
+                                    # Uses btrfs version of du
 
 alias ls="lsd --group-dirs=first"   # uber cool ls alternative
 alias tree="lsd --tree"
 
 alias cp="cp --reflink=auto"        # under btrfs, privilege reflinks
-
-alias nasmdump="objdump -M intel"
 
 alias type="type -f"                # Enable func printing on zsh type
 
@@ -21,7 +21,8 @@ alias ssh='kitty-color $c_kitty_ssh_bg $c_kitty_opacity ssh $@'
 
 alias open="xdg-open"               # Think different.
 
-alias grep="grep --color=auto"      # ADD
+alias grep="grep --color=auto"      # JUST
+alias zgrep="zgrep --color=auto"    # ADD
 alias fgrep="fgrep --color-auto"    # SOME
 alias egrep="egrep --color=auto"    # COLOUR. grep is way better with highlight.
 
@@ -29,24 +30,23 @@ alias murder="kill -9"
 alias pmurder="pkill -9"
 alias murderall="killall -9"        # SIGKILL ftw
 
-alias anihilate='shred -f -n 40 -z' # Rewrite perms, shred 15 passes and zero out
+alias anihilate='shred -f -n 40 -z' # Rewrite perms, shred 40 passes and zero out
 
 alias trash="gio trash"             # Use desktop trash in the terminal
 
-alias userctl="systemctl --user"    # Manage user-mode systemd un
+alias userctl="systemctl --user"    # Manage user-mode systemd units
 
 alias icat="kitty +kitten icat"     # kitty's image viewer
 
 alias yelp="noglob yelp"
 
 alias ssh-cremi="ssh -YCt4 kvergnes@sshproxy.emi.u-bordeaux.fr ssh -YCt trelawney zsh"
-                                    # Just zoom past the proxy and connect to mcgonagall
+                                    # Just zoom past the proxy and connect to trelawney
 
 alias ghci="LANG=C.UTF-8 ghci"
                                     # Fixes locale for Nix ghci
 
-alias btrfs-ducks="btrfs filesystem du -s --human-readable"
-                                    # Uses btrfs version of du
+
 
 alias calc="qalc"
 
