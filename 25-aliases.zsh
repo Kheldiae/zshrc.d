86634d4be1eed5ff67d85a2b579f4b16d622593a
TheSola10 <me@thesola.io>
2023-03-03 13:13:45 +0100

    Fixed python import in nix-shell

--- 25-aliases.zsh
+++ 25-aliases.zsh
@@ -13,7 +13,11 @@ alias type="type -f"                # Enable func printing on zsh type
 
 if [[ -v IN_NIX_SHELL ]]
 then
+    pyver=$(python3 --version | cut -d' ' -f 2| cut -d'.' -f 1-2)
+    repypath="PYTHONPATH=$HOME/.local/lib/python$pyver/site-packages:\$PYTHONPATH" 
     alias vim="nvim --cmd \"let g:python3_host_prog = '$OLDPYTHON'\""
+    alias ipython="$repypath ipython"
+    alias jupyter="$repypath jupyter"
 else
     alias vim="nvim"                # Switched to Neovim :D
 fi
