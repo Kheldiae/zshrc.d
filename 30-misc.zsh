853a13915b0d80c9066668bad0bcdc26716c01f6
TheSola10 <me@thesola.io>
2023-12-04 11:28:24 +0100

    Added tmux config stuff

--- 30-misc.zsh
+++ 30-misc.zsh
@@ -5,6 +5,15 @@
 if (( ${+TMUX} ))
 then
     export TERM=screen-256color         # 256color by default
+    export VISUAL="tmux display-popup -E goyo"
+    export EDITOR="tmux display-popup -E goyo"
+                                        # Popup commit editor
+
+    # We're in tmux, in kitty, use stealth settings
+    if [[ $TERMINFO =~ *kitty* ]]
+    then
+        tmux set -g status off
+    fi
 fi
 
 if [ -f $HOME/.box-name ]
