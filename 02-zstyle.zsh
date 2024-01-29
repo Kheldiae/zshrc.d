dc913646bcb99a7b20dfae70fd3391db7ce95432
TheSola10 <me@thesola.io>
2024-01-29 21:33:52 +0100

    Color syntax consistency + space before cursor

--- 02-zstyle.zsh
+++ 02-zstyle.zsh
@@ -10,13 +10,13 @@ zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %d --%f'
 zstyle ':completion:*' group-name       ''
 zstyle ':completion:*' insert-unambiguous true
 zstyle ':completion:*' list-colors      ''
-zstyle ':completion:*' list-prompt      $'%{\e[38;2;230;219;116m\e[38;5;232;48;2;230;219;116m%} SCROLL %{\e[38;5;15;48;2;48;48;48m%} At %p %{\e[38;2;48;48;48;40m%}%{\e[0m%}'
+zstyle ':completion:*' list-prompt      $'%{\e[38;2;230;219;116m\e[38;5;232;48;2;230;219;116m%} SCROLL %{\e[38;5;15;48;2;48;48;48m%} At %p %{\e[38;2;48;48;48;40m%} %{\e[0m%}'
 zstyle ':completion:*' matcher-list     '' \
                                         'm:{[:lower:]}={[:upper:]} l:|=* r:|=*' \
                                         'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
 zstyle ':completion:*' menu             select=long-list select=0 select=0
 zstyle ':completion:*' original         true
-zstyle ':completion:*' select-prompt   $'%{\e[38;2;128;160;255m\e[38;5;232;48;2;128;160;255m%} SELECT %{\e[38;5;15;48;2;48;48;48m%} At %p %{\e[0;38;2;48;48;48m%}%{\e[0m%}'
+zstyle ':completion:*' select-prompt   $'%{\e[38;2;128;160;255m\e[38;5;232;48;2;128;160;255m%} SELECT %{\e[38;5;15;48;2;48;48;48m%} At %p %{\e[38;2;48;48;48;40m%}%{\e[0m%}'
 zstyle ':completion:*' verbose          true
 zstyle ':completion:*' file-list        all
 zstyle ':compinstall'  filename         "$ZSH_CONFIG_PATH/02-zstyle.zsh"
