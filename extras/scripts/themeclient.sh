066acf6e6f020c4ba4ec897a34fb200be2b7a991
TheSola10 <me@thesola.io>
2024-02-03 12:46:50 +0100

    Fixed themeclient predicate

--- extras/scripts/themeclient.sh
+++ extras/scripts/themeclient.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-[[ -v SSH_TTY ]] || exit 0
+
+# Don't clobber the themeserver and day/night process of the desktop
+[[ $XDG_SESSION_DESKTOP == gnome ]] && exit 0
+[[ $XDG_SESSION_DESKTOP == KDE ]] && exit 0
 
 RUNDIR=${XDG_RUNTIME_DIR:-/tmp/run-$UID}
 
 rm -f $RUNDIR/theme
 mkfifo $RUNDIR/theme
 
+echo "Starting retrieval of theme information on port 19915..."
 while nc -n 127.0.0.1 19915 </dev/null >$RUNDIR/theme
 do true
 done
